354719c7cfb20663f7b4a81d450f2958cf49a64c
davin <davinmagic3@gmail.com>
2025-09-08 19:11:13 +0700

    MVT complete

---
+++ main/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Store(models.Model):
+    name = models.CharField(max_length=255)  # item name
+    price = models.IntegerField()  # item price
+    description = models.TextField()  # item description
+    thumbnail = models.URLField(max_length=500)  # item image URL
+    category = models.CharField(max_length=100)  # item category
+    is_featured = models.BooleanField(default=False)  # featured status
+
+    def __str__(self):
+        return self.name
